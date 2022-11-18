0deb51fb940fbc2b248cee99637cd9c7c7af3b9b
PaulaDuna <paula@dunayevich.com.ar>
2022-11-18 12:24:54 -0300

    Add keywords_finder.py to find news articles containing keywords
    Returns two lists with the articles found and their corresponding dates.

---
+++ keywords_finder.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+import json
+
+with open("data/dates_2020.json", 'r') as f:
+  dates = json.load(f)
+
+with open("data/titles_2020.json", 'r') as f:
+  titles = json.load(f)
+
+with open("data/articles_2020.json", 'r') as f:
+  articles = json.load(f)
+
+keywords = ["coronavirus", "covid", "cov-2", "cuarentena"]
+dates_search = []
+titles_search = []
+articles_search = []
+
+for date, title, article in zip(dates, titles, articles):
+  for word in keywords:
+    if word.lower() in article.lower() and title not in titles_search:
+      dates_search.append(date)
+      articles_search.append(article)
+
+with open("data/dates_search_2020.json", 'w') as f:
+  json.dump(dates_search, f, indent = 2) 
+
+with open("data/articles_search_2020.json", 'w') as f:
+  json.dump(articles_search, f, indent = 2) 
